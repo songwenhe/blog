af96b6d4a597ad55e32ced734a8b335307f96d09
myqz <ambition292@163.com>
2022-03-20 22:26:17 +0800

    update IMG_URL

--- src/utils/global/index.js
+++ src/utils/global/index.js
@@ -1,4 +1,4 @@
-export const IMG_URL = 'http://localhost:8082/file/'
+export const IMG_URL = 'http://localhost:8088/'
 export const BASE_URL = 'http://localhost:8088/'
 export const POST_STATE = [
   { type: 0, text: '私密' },
